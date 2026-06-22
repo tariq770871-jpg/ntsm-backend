3d6abf2b83751ed6af72fa9128ae1875d2d86525
tariq770871-jpg <tariq770871@gmail.com>
2026-06-22 07:35:20 +0300

    Add JwtModule to MessagesModule

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
 import { MessagesGateway } from './messages.gateway';
 import { Message } from './message.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
+  imports: [
+    TypeOrmModule.forFeature([Message]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET') || 'default-secret',
+      }),
+    }),
+  ],
   providers: [MessagesService, MessagesGateway],
   controllers: [MessagesController],
   exports: [MessagesService],
