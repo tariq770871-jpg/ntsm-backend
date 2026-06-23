e87e5e1c283286695e5fc74619eb50a639ebe684
tariq770871-jpg <tariq770871@gmail.com>
2026-06-23 03:11:04 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(helmet());
   app.use(compression());
   app.enableCors({
-    origin: process.env.FRONTEND_URL || '*',
+    origin: ['https://vip-market.vercel.app', process.env.FRONTEND_URL].filter(Boolean),
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({
